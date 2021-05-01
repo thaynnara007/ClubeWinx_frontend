92f352215a16eb930f7a4c8e6cdbab2fa175fba2
Thaynnara <thaynnara.goncalves@ccc.ufcg.edu.br>
2021-05-01 12:07:24 -0300

    teste

--- homemate/src/App.js
+++ homemate/src/App.js
@@ -5,6 +5,7 @@ function App() {
   console.log("teste")
   console.log("teste")
   console.log("teste")
+  console.log("teste")
   return (
     <div className="App">
       <header className="App-header">
