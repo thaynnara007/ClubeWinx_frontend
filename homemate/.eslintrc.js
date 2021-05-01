ba4d8869c902cef6c7eed19fdd8fe29de7da4df1
Thaynnara <thaynnara.goncalves@ccc.ufcg.edu.br>
2021-05-01 13:05:48 -0300

    add config files

--- homemate/.eslintrc.js
+++ homemate/.eslintrc.js
@@ -3,18 +3,44 @@ module.exports = {
     commonjs: true,
     es6: true,
     node: true,
+    browser: true,
   },
-  plugins: ["import"],
-  extends: "eslint:recommended",
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
+  parser: 'babel-eslint',
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
+  plugins: ['react', 'prettier'],
   rules: {
-    "no-console": "warn",
-    "import/first": "error"
+    'no-console': 'warn',
+    'global-require': 'off',
+    'no-throw-literal': 'off',
+    'no-nested-ternary': 'off',
+    'prettier/prettier': 'error',
+    'import/no-dynamic-require': 'off',
+    'import/prefer-default-export': 'off',
+
+    'react/prop-types': 'off',
+    'react/no-unescaped-entities': 'off',
+    'react/jsx-curly-newline': 'off',
+    'no-irregular-whitespace': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/destructuring-assignment': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+
+    'jsx-a11y/anchor-is-valid': 'off',
+    'jsx-a11y/control-has-associated-label': 'off',
+    'jsx-a11y/mouse-events-have-key-events': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
   },
-};
+}
--- homemate/src/App.js
+++ homemate/src/App.js
@@ -1,15 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
-  console.log("teste")
-  console.log("teste")
-  console.log("teste")
-  console.log("teste")
-  console.log("teste")
-  console.log("teste")
-  console.log("teste")
-  console.log("teste")
   return (
     <div className="App">
       <header className="App-header">
@@ -27,7 +19,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
