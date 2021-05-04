3a9776f89935a347762891b394e934c22cf431d1
Helena Dantas <helena.dantas@ccc.ufcg.edu.br>
2021-05-03 23:09:44 -0300

    Correção na requisição

--- homemate/src/components/form/editRegister/index.js
+++ homemate/src/components/form/editRegister/index.js
@@ -137,7 +137,6 @@ function EditRegister (){
           phoneNumber,
           birthday,
           gender,
-          password,
           street,
           number,
           district,
@@ -162,6 +161,8 @@ function EditRegister (){
         } 
     
     );
+
+    
 
     return (
         <div style={{ marginTop: '150px' }}>
