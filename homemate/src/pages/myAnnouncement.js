ad1fc4028de1e0383e768b5e35be4a2437ad0f8d
Mariana Araujo <mariana.a.lucena@accenture.com>
2021-05-03 22:47:39 -0300

    Iniciando a criação de anúncio

--- homemate/src/pages/myAnnouncement.js
+++ homemate/src/pages/myAnnouncement.js
@@ -1,5 +1,50 @@
+import BaseButton from '../components/button/baseButton';
+  
+import { useHistory } from "react-router";
+import BasicForm from '../components/form/BasicForm';
+import Announcementent from '../components/form/announcement';
+
 function MyAnnouncemente() {
-  return <div>My Announcemente</div>;
+  const history = useHistory();
+  const response = true;
+
+
+  const handleClick = () => {
+      history.push('/newAnnouncement');
+  }
+
+  const existAnnouncement = () => {
+   
+    /*
+    api
+        .post('/user/poster/my', body)
+        .then((response) => {
+          const { token, user } = response.data;
+
+          localStorage.setItem('homemate_access_token', token);
+          toast(`Bem-vindo de volta ${user.name}!`);
+        })
+        .catch((error) => {
+          let msg = '';
+          if (error.response) msg = error.response.data.error;
+          else msg = 'Network failed';
+
+          toast.error(msg);
+        });
+        */
+      
+  }
+
+  return <div style={{ marginTop: '150px' }}>
+    <BasicForm>
+      {response && <BaseButton onClick={handleClick}>Inserir Anúncio</BaseButton>}
+    </BasicForm>
+  </div>;
+  
 }
+
+
+
+
 
 export default MyAnnouncemente;
