d3c7b28f096e754f491f467db09b61eb96f10221
Thaynnara <thaynnara.goncalves@ccc.ufcg.edu.br>
2021-07-01 20:45:53 -0300

    add infoSpan

--- homemate/src/App.jsx
+++ homemate/src/App.jsx
@@ -1,18 +1,11 @@
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import IconBed from './components/icons/iconBed'
-import IconPeople from './components/icons/iconPeople'
-import IconDoor from './components/icons/iconDoor'
-import IconBath from './components/icons/iconBath'
 
 toast.configure();
 function App() {
   return (
     <>
-      <IconPeople/>
-      <IconBath/>
-      <IconDoor/>
-      <IconBed/>
+      <h1>Hello</h1>
       <ToastContainer />
     </>
   );
---
+++ homemate/src/components/infoSpan/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import './infoSpan.css';
+
+function InfoSpan({
+  amountText = '0',
+  amountTextSize = '36px',
+  description,
+  descriptionSize = '14px',
+  spanColor = '#6983AA',
+  children,
+}) {
+  return (
+    <div className="info-span-info">
+      <span
+        style={{
+          fontSize: amountTextSize,
+          fontWeight: '700',
+          fontFamily: 'Roboto',
+          color: spanColor,
+        }}
+      >
+        {amountText}
+      </span>
+      <div style={{ marginLeft: '0.3em' }}>
+        {children}
+        <span
+          className="info-span-icon-span"
+          style={{ color: spanColor, fontSize: descriptionSize }}
+        >
+          {description}
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default InfoSpan;
