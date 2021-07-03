8d6a7931ce54d7b624564b1d4b3418e6f1631271
Thais Araujo <thais.toscano@ccc.ufcg.edu.br>
2021-07-03 13:57:14 -0300

    Text Component

---
+++ homemate/src/components/text/index.js
@@ -0,0 +1,10 @@
+import style from './text.css';
+
+function Text({ children , styles }) {
+
+    return (
+        <p styles={styles={style}, styles}>{children}</p>
+    );
+}
+
+export default Text;
