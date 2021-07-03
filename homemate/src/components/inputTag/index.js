3db5d1243cb522f49c29a3ff6dbd572aea8cf2b8
Gabriel Valentino <gabriel.alves@ccc.ufcg.edu.br>
2021-07-03 15:57:49 -0300

    InputTag

---
+++ homemate/src/components/inputTag/index.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import './inputTag.css';
+
+function InputTag(props) {
+  const { children, clickTag, styles } = props;
+  const [tagValue, setTagValue] = useState(truncateTag(children));
+  const [expanseTag, setExpanseTag] = useState("(...)");
+  const [truncate, setTruncate] = useState(false);
+
+  function truncateTag(tag) {
+    if(tag.length > 10) { 
+      return  children.slice(0,9)
+    } else {
+      return tag;
+    }
+  }
+
+  function changeTag(tag) {
+    setTruncate(true);
+    if(tag != children) { 
+      setTagValue(children);
+      setExpanseTag('<<<');
+    } else {
+      setTagValue(children.slice(0,9));
+      setExpanseTag('(...)');
+    }
+  }
+
+  return (
+    <div class='components-inputTag-tag' style={styles?? {}}>
+      <span onClick={() => clickTag(children)}>
+        {tagValue} 
+      </span>
+      {truncate || children != tagValue ? <span onClick={() => changeTag(tagValue)}>{expanseTag}</span> : ""} 
+    </div>
+  );
+}
+export default InputTag;
