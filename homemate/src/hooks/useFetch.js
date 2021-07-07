138f6894e8835cf7e3971d397ffeb4358bde4fd0
Silva <wesley.santos.silva@accenture.com>
2021-07-07 10:10:49 -0300

    add loading component for useFetch requesitions

--- homemate/src/hooks/useFetch.js
+++ homemate/src/hooks/useFetch.js
@@ -4,22 +4,25 @@ import api from '../api';
 
 function useFetch(url) {
   const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     api
       .get(url)
       .then((response) => {
+        setIsLoading(false);
         setData(response.data);
       })
       .catch((error) => {
         let msg = '';
         if (error.response) msg = error.response.data.error;
         else msg = 'Network failed';
+        setIsLoading(false);
         toast.error(msg);
       });
   }, []);
 
-  return { data };
+  return { data, isLoading };
 }
 
 export default useFetch;
