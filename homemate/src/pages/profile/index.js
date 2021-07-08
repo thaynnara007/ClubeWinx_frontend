cc7eadc4d0c69756a4b01cccd99c23def944cec6
Thaynnara <thaynnara.goncalves@ccc.ufcg.edu.br>
2021-07-08 15:43:22 -0300

    add avatar component

--- homemate/src/pages/profile/index.js
+++ homemate/src/pages/profile/index.js
@@ -1,17 +1,34 @@
 import React, { useState } from 'react';
+import { Avatar } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
 
 import './profile.css';
+
+const useStyles = makeStyles(() => ({
+  avatar: {
+    width: 200,
+    height: 200,
+    backgroundColor: '#8566AA',
+    fontSize: 40,
+    zIndex: 3,
+    position: 'relative',
+    left: '-50%',
+  },
+}));
 
 function Profile() {
   const [headerBackground, setHeaderBackground] = useState({ backgroundColor: '#D9D4DF' });
   const [isHeaderImage, setIsHeaderImage] = useState(false);
 
+  const styles = useStyles();
+
   return (
     <>
       <div className="profile-header" style={{ ...headerBackground }}>
-        <div className="profile-image"/>  
-        <div className="profile-box">
+        <div style={{ position: 'absolute', left: '50%', top: '13%' }}>
+          <Avatar className={styles.avatar}>TH</Avatar>
         </div>
+        <div className="profile-box"></div>
       </div>
     </>
   );
