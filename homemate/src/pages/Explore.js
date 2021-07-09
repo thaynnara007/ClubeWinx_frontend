6792402cd2afaee630331dfc3e884885eba34952
Thais Araujo <thais.toscano@ccc.ufcg.edu.br>
2021-07-09 14:36:43 -0300

    exibir fotos

--- homemate/src/pages/Explore.js
+++ homemate/src/pages/Explore.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import '../components/picture/picture.css';
+import Picture from '../components/picture';
 
 function Explore() {
-  return <p>Explore</p>;
+  const posts = [
+    {
+      id: 1,
+      post: 'https://dmhxz00kguanp.cloudfront.net/fotos/136023/quarto-infantil-estrelinha-rosa-291044.jpg',
+      legend: 'Quarto',
+    },
+    {
+      id: 2,
+      post: 'https://images-americanas.b2w.io/produtos/01/00/img/84842/9/84842978_1GG.jpg',
+      legend: 'Banheiro',
+    },
+  ];
+
+  return (
+    <div className="Picture">
+      <h1>Show Picture Component of Homemate System</h1>
+      <h3>Componente Exibir Foto do Sistema Homemate</h3>
+      <Picture listPost={posts} />
+    </div>
+  );
 }
 
 export default Explore;
