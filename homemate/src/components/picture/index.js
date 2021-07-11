1010ce0e596795dcfac1a47fdf4e3313bd3ff69a
Thaynnara <thaynnara.goncalves@ccc.ufcg.edu.br>
2021-07-11 20:15:16 -0300

    adding dependency

--- homemate/src/components/picture/index.js
+++ homemate/src/components/picture/index.js
@@ -3,17 +3,16 @@ import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 function Picture({ listPost }) {
-
   return (
     <Carousel showArrows infiniteLoop showStatus={false} showIndicators={false} showThumbs={false}>
       {listPost &&
-            listPost.length > 0 &&
-            listPost.map((post) => (
-              <div>
-                <img alt="" src={post.post} />
-                <p className="legend">{post.legend}</p>
-              </div>
-            ))}
+        listPost.length > 0 &&
+        listPost.map((post) => (
+          <div>
+            <img alt="" src={post.post} />
+            <p className="legend">{post.legend}</p>
+          </div>
+        ))}
     </Carousel>
   );
 }
