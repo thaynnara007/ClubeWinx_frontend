8f939a090270740be1c59d33de2a2c4768e2361f
Gabriel Valentino <gabriel.alves@ccc.ufcg.edu.br>
2021-07-24 11:14:38 -0300

    add tags no perfil

---
+++ homemate/src/components/autocomplete/index.js
@@ -0,0 +1,111 @@
+import React, { Fragment, useState  } from "react";
+import useFetch from '../../hooks/useFetch';
+import Input from '../input';
+import Button from '../button';
+
+import "./autocomplete.css";
+
+function Autocomplete(props) {
+  const { tags, setTags, creatTag} = props
+  const { data: suggestions } = useFetch('/tag');
+
+  const [filteredSuggestions, setFilteredSuggestions] = useState([]);
+  const [userInput, setUserInput] = useState('');
+  const [category, setCategory] = useState(1);
+
+  const onChange = input => {
+    const filteredSuggestionsChange = input
+    ?
+    suggestions.filter(
+      suggestion =>
+      suggestion.name.toLowerCase().indexOf(input.toLowerCase()) > -1 && suggestion.categoryId == category
+      )
+      :
+      [];
+      
+    setFilteredSuggestions(filteredSuggestionsChange);
+    setUserInput(input);
+  };
+
+  const onClick = e => {
+    setFilteredSuggestions([]);
+    setUserInput("");
+    
+    const tagId = e.currentTarget.value;
+    const tag = suggestions.find(input => input.id === tagId); 
+
+    if(!tags.includes(tag)){
+      setTags((prev) => [...prev, tag])
+    }
+  };
+
+  const changeCategory = e => {
+    console.log(e.currentTarget.value);
+    setCategory(e.currentTarget.value);
+  };
+
+  const suggestionsListComponent = (filteredSuggestions.length > 0)
+    ?
+    <ul class="suggestions">
+      {filteredSuggestions.map((suggestion, index) => {
+
+        return (
+          <li value={suggestion.id} key={index} onClick={onClick}>
+            {suggestion.name}
+          </li>
+        );
+      })}
+    </ul>
+    :
+    <div className="no-suggestions">
+      <em>Top tags</em>
+    </div>
+
+
+  const newTag = () => {
+    const inputName = userInput;
+    const tag = {
+      id: -1,
+      name: inputName,
+      categoryId: category
+    }
+    console.log(tag)
+    if(tag){
+      setTags((prev) => [...prev, tag])
+    }
+  };
+
+  return (
+    <>
+
+      <Fragment>
+      <Input
+        name="Buscar tags"
+        value={userInput}
+        onChange={onChange}
+      />
+        {suggestionsListComponent}
+      </Fragment>
+      {creatTag 
+      ?
+      <>
+      <Button onClick={newTag}>Criar Tag</Button>
+      </>
+      :
+      []
+      }
+      <select onChange={changeCategory} value={category.toString()}>
+        <option value="1">Moradia</option>
+        <option value="2">Saúde</option>
+        <option value="3">Estudante</option>
+        <option value="4">Curso</option>
+        <option value="5">Animais</option>
+        <option value="6">Minorias</option>
+        <option value="7">Sobre você</option>
+      </select>
+
+    </>
+  );
+}
+
+export default Autocomplete;
--- homemate/src/components/profileEdit/index.js
+++ homemate/src/components/profileEdit/index.js
@@ -5,7 +5,12 @@ import { useHistory } from 'react-router';
 import api from '../../api';
 import Input from '../input';
 import BaseButton from '../button';
+import Autocomplete from '../autocomplete';
+import InputTag from '../inputTag';
+import ScrollBox from '../scrollBox';
+
 import Loading from '../loading';
+import { getTagColor } from '../../utils/functions';
 
 const stylesInvalid = {
   label: {
@@ -19,12 +24,35 @@ const stylesValid = {
   },
 };
 
+
+const tagsBoxStyle = {
+  display: {
+    gridColumn: '1 / 3',
+    gridRow: '5 / 7',
+    width: '100%',
+    height: '100%',
+    border: '2px solid #cbdae5',
+    borderRadius: '8px',
+    justifySelf: 'center',
+    marginTop: '40px',
+  },
+  item: {
+    height: 'fit-content',
+  },
+};
+
 function ProfileEdit({ profile }) {
   const [description, setDescription] = useState(profile?.description ?? '');
   const [problemDescription, setProblemDescription] = useState({});
   const [socialMedia, setSocialMedia] = useState(profile?.socialMedia ?? '');
   const [problemSocialMedia, setProblemSocialMedia] = useState({});
   const [loading, setLoading] = useState(false);
+
+  const [tagsElement, setTagsElement] = useState([]);
+  const [profileTag, setProfileTag] = useState([]);
+  const addTagPerfilURL = '/profile/me/add/tags';
+  const removeTagPerfilURL = '/profile/me/remove/tags';
+  const createTag = '/profile//me/create/tags';
 
   useEffect(() => {
     function updateState() {
@@ -57,6 +85,7 @@ function ProfileEdit({ profile }) {
     const validated = validateInfo();
 
     if (validated) {
+      addTagP();
       const body = {
         description,
         socialMedia,
@@ -79,6 +108,82 @@ function ProfileEdit({ profile }) {
     }
   };
 
+  const addTagP = () => {
+    console.log("addTagas");
+    const add = tagsElement.filter((tag) => tag.id !== -1);
+    const createTags = tagsElement.filter((tag) => tag.id === -1);
+    create(createTags);
+    const tags = add.map((tag) => tag.id);
+    console.log("tagas q vao ser adicionadas no perfil ",tags);
+    const body = {
+      tags
+    }
+    api
+    .post(addTagPerfilURL, body)
+    .then((response) => {
+      setProfileTag(response.data.tags);
+      // attPtags();
+    })
+    .catch((error) => {
+      console.log('error'. error);
+      let msg = '';
+      if (error.response) msg = error.response.data.error;
+      else msg = 'Network failed add';
+    });
+  }
+
+  function removeTagsP (tagId) {
+    const tags = [tagId];
+    const body = {
+      tags
+    }
+    api
+    .put(removeTagPerfilURL, body)
+    .then((response) => {
+      setProfileTag(response.data.tags);
+      console.log(profileTag)
+    })
+    .catch((error) => {
+      let msg = '';
+      if (error.response) msg = error.response.data.error;
+      else msg = 'Network failed remove';
+    });
+  }
+
+  const create = (tags) => {
+    if (tags && tags.length > 0) {
+      tags.forEach(element => {
+        console.log(element)
+        const body = {
+          'tags': [
+            {
+              'categoryId': element.categoryId,
+              'name': element.name
+            }
+          ]
+        };
+        api 
+          .post(createTag, body)
+          .then((response) => {
+            console.log(response)
+            setMyTags(response.data.tags);
+            setMyTagsList(response.data.tags);
+            toast('Anúncio criado com su  cesso');
+            toast('Edite seu anúncio: adicione algumas tags!');
+            setFlag(true);
+            setStateAnnouncement(ENTER_PAGE_MYANNOUNCEMENTT)
+          })
+          .catch((error) => {
+            let msg = '';
+            if (error.response) msg = error.response.data.error;
+            else msg = 'Network failed';
+            toast.error(msg);
+          });
+        
+      });
+    }
+  }
+
   return (
     <>
       {loading ? (
@@ -99,9 +204,42 @@ function ProfileEdit({ profile }) {
             onChange={setSocialMedia}
             styles={problemSocialMedia}
           />
+        {
+          profileTag && profileTag.length > 0 && profileTag.map(x => {
+            const tagColor = getTagColor(x.categoryId);
+            return(
+            <InputTag styles={{ backgroundColor: `${tagColor}`}}>{x.name}</InputTag>);
+          })
+        }
+          <Autocomplete creatTag={true} tags={tagsElement} setTags={setTagsElement} />
           <BaseButton onClick={editProfile} styles={{ width: '100%', fontWeight: 'bold' }}>
             SALVAR
           </BaseButton>
+          <ScrollBox styles={tagsBoxStyle}>
+              {profile?.tags
+                ? profile?.tags?.map((tag, index) => {
+                    const tagColor = getTagColor(tag?.categoryId);
+                    return (
+                      <InputTag
+                        key={index}
+                        styles={{ backgroundColor: `${tagColor}` }}
+                      >{`${tag?.name}`}</InputTag>
+                    );
+                  })
+                : []}
+                {tagsElement
+                ? tagsElement?.map((tag, index) => {
+                    const tagColor = getTagColor(tag?.categoryId);
+                    return (
+                      <InputTag
+                        key={index}
+                        styles={{ backgroundColor: `${tagColor}` }}
+                      >{`${tag?.name}`}</InputTag>
+                    );
+                  })
+                : []}
+            </ScrollBox>
+
         </>
       )}
     </>
