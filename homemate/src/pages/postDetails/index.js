be89dcb831add5ce09ec478e5eaef80f757314a8
Thaynnara <thaynnara.goncalves@ccc.ufcg.edu.br>
2021-07-24 08:24:14 -0300

    fix2

--- homemate/src/pages/postDetails/index.js
+++ homemate/src/pages/postDetails/index.js
@@ -259,19 +259,19 @@ function PostDetails() {
             <div className="post-info-display">
               <ScrollBox styles={scrollBoxIconsStyles}>
                 {[
-                  <InfoSpan description="pessoas" amountText={post?.residents}>
+                  <InfoSpan description="pessoas" amountText={post?.residents ?? ''}>
                     <IconPeople />
                   </InfoSpan>,
                   <div className="flip-card-back-vl" />,
-                  <InfoSpan description="quartos" amountText={post?.vacancies}>
+                  <InfoSpan description="quartos" amountText={post?.vacancies ?? ''}>
                     <IconDoor />
                   </InfoSpan>,
                   <div className="flip-card-back-vl" />,
-                  <InfoSpan description="camas" amountText={post?.beds}>
+                  <InfoSpan description="camas" amountText={post?.beds ?? ''}>
                     <IconBed />
                   </InfoSpan>,
                   <div className="flip-card-back-vl" />,
-                  <InfoSpan description="banheiros" amountText={post?.bathrooms}>
+                  <InfoSpan description="banheiros" amountText={post?.bathrooms ?? ''}>
                     <IconBath />
                   </InfoSpan>,
                 ]}
