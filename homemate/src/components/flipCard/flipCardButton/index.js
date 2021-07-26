92f38e9683954396abe47243ce3fb9b549c13bac
Thaynnara <thaynnara.goncalves@ccc.ufcg.edu.br>
2021-07-26 20:56:33 -0300

    add flip card button

---
+++ homemate/src/components/flipCard/flipCardButton/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './flipCardButton.css';
+
+function FlipCardButton({ buttonName }) {
+  const imageStyles = {
+    backgroundImage: `linear-gradient(40deg, rgba(105, 131, 170, 0.7),rgba(244, 244, 244, 0.7))`,
+  };
+
+  return (
+    <div className="flip-card-plus-card" style={imageStyles}>
+      {buttonName}
+    </div>
+  );
+}
+
+export default FlipCardButton;
