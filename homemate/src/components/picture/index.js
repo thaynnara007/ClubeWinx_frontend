55ea2584ba32abaf82d4cc9308837fa6f19686d3
Thaynnara <thaynnara.goncalves@ccc.ufcg.edu.br>
2021-07-26 20:59:27 -0300

    updates picture with flip card button

--- homemate/src/components/picture/index.js
+++ homemate/src/components/picture/index.js
@@ -1,21 +1,47 @@
 /* eslint-disable react/no-array-index-key */
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
+import FlipCardButton from '../flipCard/flipCardButton';
+import FileUploader from '../fileUploader';
+import IconPlus from '../icons/iconPlus';
+import './picture.css';
+
 function Picture({ listPost }) {
+  const [headerBackground, setHeaderBackground] = useState({ backgroundColor: '#D9D4DF' });
+
+  const handleHeaderUpload = (file) => {
+    if (file) {
+      const image = URL.createObjectURL(file);
+
+      setHeaderBackground({ backgroundImage: `url('${image}')` });
+    }
+  };
+
   return (
-    <Carousel showArrows infiniteLoop showStatus={false} showIndicators={false} showThumbs={false}>
+    <Carousel showArrows infiniteLoop showStatus={false} showThumbs={false}>
       {listPost &&
         listPost.length > 0 &&
-        listPost.map((post, index) => (
-          <div key={index}>
-            <img alt="" src={post.pictureUrl} />
-          </div>
-        ))}
+        listPost.map((post, index) =>
+          post.id !== -1 ? (
+            <div key={index}>
+              <img alt="" src={post.pictureUrl} />
+            </div>
+          ) : (
+            <div key={index} className="background-carrossel" style={{ ...headerBackground }}>
+              <FlipCardButton
+                buttonName={
+                  <FileUploader icon handleUpload={handleHeaderUpload}>
+                    <IconPlus size="3x" styles={{ color: '#F4F4F4' }} />
+                  </FileUploader>
+                }
+              />
+            </div>
+          )
+        )}
     </Carousel>
   );
 }
-
 export default Picture;
